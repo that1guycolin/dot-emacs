4d0d721c4ea5fd0692dc6f83502fb51ae1358ae4
Colin Loeffler <that1guycolin@gmail.com>
2026-02-28 19:27:32 -0600

    Added CI workflow and predefined treesit language grammars.

--- init.el.d/language-specific-configs.el
+++ init.el.d/language-specific-configs.el
@@ -21,7 +21,35 @@
 (use-package treesit
   :ensure nil
   :no-require t
-  :demand t)
+  :demand t
+  :custom
+  (treesit-language-source-alist
+   '((bash "https://github.com/tree-sitter/tree-sitter-bash")
+     (cmake "https://github.com/uyha/tree-sitter-cmake")
+     (css "https://github.com/tree-sitter/tree-sitter-css")
+     (fish "https://github.com/ram02z/tree-sitter-fish")
+     (emacs-lisp "https://github.com/Wilfred/tree-sitter-elisp")
+     (go "https://github.com/tree-sitter/tree-sitter-go")
+     (html "https://github.com/tree-sitter/tree-sitter-html")
+     (javascript "https://github.com/tree-sitter/tree-sitter-javascript"
+		 "master" "src")
+     (json "https://github.com/tree-sitter/tree-sitter-json")
+     (lua "https://github.com/MunifTanjim/tree-sitter-lua")
+     (make "https://github.com/alemuller/tree-sitter-make")
+     (markdown "https://github.com/tree-sitter-grammars/tree-sitter-markdown"
+	       "split_parser" "tree-sitter-markdown/src")
+     (markdown-inline
+      "https://github.com/tree-sitter-grammars/tree-sitter-markdown"
+      "split_parser" "tree-sitter-markdown-inline/src")
+     (powershell "https://github.com/airbus-cert/tree-sitter-powershell")
+     (python "https://github.com/tree-sitter/tree-sitter-python")
+     (rust "https://github.com/tree-sitter/tree-sitter-rust")
+     (toml "https://github.com/ikatyang/tree-sitter-toml")
+     (tsx "https://github.com/tree-sitter/tree-sitter-typescript"
+	  "master" "tsx/src")
+     (typescript "https://github.com/tree-sitter/tree-sitter-typescript"
+		 "master" "typescript/src")
+     (yaml "https://github.com/ikatyang/tree-sitter-yaml"))))
 
 (use-package treesit-langs
   :ensure nil
@@ -32,7 +60,7 @@
   :demand t
   :functions global-treesit-auto-mode
   :config
-  (global-treesit-auto-mode 1))
+  (global-treesit-auto-mode))
 
 ;;; Flycheck linters:
 ;; bash: 'shellcheck' (pacman -S shellcheck)
