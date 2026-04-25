34dffb847a765e62d6876b2fbfc74bebea7bcdd5
Colin Loeffler <that1guycolin@gmail.com>
2026-04-25 03:49:09 -0500

    Delete: Removed package projectile.

--- init.el.d/05-project-management.el
+++ init.el.d/05-project-management.el
@@ -10,85 +10,14 @@
 
 ;;; Code:
 ;; =======  PROJECT SUPPORT  =======
-;; `projectile' (project manager)
 ;; `disproject' (assist navigation btwn projects)
 ;; `editorconfig' (support .editorconfig)
 ;; =================================
-(keymap-global-unset "C-x p")
-(declare-function dirvish "dirvish")
-(declare-function transient-define-prefix "transient")
-(use-package projectile
-  :functions
-  projectile-mode project-projectile projectile-project-root user/no-ctags-for-sh
-  user/dirvish-at-project-root user/projectile-ignore-elpaca-packages
-  user/projectile-commander-dispatch projectile-ag projectile-ripgrep
-  projectile-reset-known-projects
 
   :custom
-  (projectile-project-search-path `(,user/projects-directory
-				    ,user/scripts-directory
-				    ,org-directory
-				    ,(expand-file-name user-emacs-directory)))
-  (projectile-completion-system 'default)
-  (projectile-track-known-projects-automatically t)
-  (projectile-enable-caching 'persistent)
-  (projectile-indexing-method 'alien)
-
   :config
   (dolist (dir '("^node_modules$" "^\\.venv$" "^\\.uv$"))
-    (add-to-list 'projectile-globally-ignored-directories dir))
   
-  (projectile-mode +1)
-  (add-hook 'project-find-functions #'project-projectile)
-
-  (defun user/no-ctags-for-sh ()
-    "Prevent creation of ctags for shell script files.
-This function sets local values of `projectile-tags-*' variables to nil."
-    (setq-local
-     projectile-tags-backend nil
-     projectile-tags-command nil
-     projectile-tags-file-name nil))
-  (add-hook 'sh-mode-hook #'user/no-ctags-for-sh)
-
-  (defun user/dirvish-at-project-root ()
-    "Open dirvish at the current Projectile project root."
-    (let ((root (projectile-project-root)))
-      (dirvish root)))
-  (setq projectile-switch-project-action #'user/dirvish-at-project-root)
-
-  (defun user/projectile-ignore-elpaca-packages (project-root)
-    "Return non-nil if PROJECT-ROOT is inside the Elpaca directory."
-    (let ((elpaca-dir (expand-file-name "elpaca/" user-emacs-directory)))
-      (string-prefix-p elpaca-dir project-root)))
-  (setq projectile-ignored-project-function
-        #'user/projectile-ignore-elpaca-packages)
-
-  (defvar user/projectile-commander-dispatch nil)
-  (transient-define-prefix
-    user/projectile-commander-dispatch ()
-    "Replace Projectile Commander with a transient buffer.
-The keybindings are exactly the same."
-    ["Projectile Commander"
-     [("D" "Open project root in dired." projectile-dired)
-      ("R" "Regerate the project's [e|g] tags." projectile-regenerate-tags)
-      ("T" "Find test file in project." projectile-find-test-file)
-      ("V" "Browse dirty projects" projectile-browse-dirty-projects)]
-     [("a" "Run ag on a project."  (lambda () (interactive)
-				     (call-interactively #'projectile-ag)))
-      ("b" "Switch to project buffer." projectile-switch-to-buffer)
-      ("d" "Find directory in project." projectile-find-dir)
-      ("e" "Find recently visited file in project." projectile-recentf)]
-     [("f" "Find file in project." projectile-find-file)
-      ("g" "Run grep on project." projectile-grep)
-      ("j" "Find tag in project." projectile-find-tag)
-      ("k" "Kill all project buffers." projectile-kill-buffers)]
-     [("o" "Run mutli-occur on project buffers." projectile-multi-occur)
-      ("p" "Run ripgrep on project." (lambda () (interactive)
-				       (call-interactively
-					#'projectile-ripgrep)))
-      ("r" "Replace a string in the project." projectile-replace)
-      ("s" "Switch project." projectile-switch-project)]
-     [("v" "Open project root in vc-dir or magit." projectile-vc)]])
   (bind-keys
    :map ctl-x-map
    ("p" . user/projectile-commander-dispatch)))
@@ -138,7 +67,6 @@ The keybindings are exactly the same."
    ("C-x p f" . treemacs-project-follow-mode)
    ("<backspace>" . treemacs-root-up)))
 
-(use-package treemacs-projectile
   :after treemacs)
 
 (use-package treemacs-nerd-icons
@@ -147,26 +75,14 @@ The keybindings are exactly the same."
   :config
   (treemacs-nerd-icons-config))
 
-(defvar user/projectile-treemacs-anywhere-dispatch nil)
-(declare-function user/projectile-treemacs-anywhere-dispatch
-		  "05-project-management.el")
 (transient-define-prefix
-  user/projectile-treemacs-anywhere-dispatch ()
-  "Globally available commands for Treemacs & Projectile."
   [
    ["Treemacs" :pad-keys t
     ("t" "Toggle" treemacs)
     ("T" "Refresh" treemacs-refresh)
-    ("j" "treemacs-projectile" treemacs-projectile)]
 
    ["Treemacs - Current View"
     ("v f" "Focus to active file" treemacs-find-file)
-    ("v p" "Add project" treemacs-add-project-to-workspace)
-    ("v a" "Add active project"
-     treemacs-add-and-display-current-project)
-    ("v c" "Collapse" treemacs-collapse-all-projects)
-    ("v r" "Reset view (current project only)"
-     treemacs-add-and-display-current-project-exclusively)]
    
    ["Treemacs - Workspaces"
     ("w e" "Edit" treemacs-edit-workspaces)
@@ -175,25 +91,6 @@ The keybindings are exactly the same."
     ("w r" "Rename" treemacs-rename-workspace)
     ("w d" "Delete" treemacs-remove-workspace)]
    
-   ["Projectile"
-    ("i" "Info" projectile-project-info)
-    ("o" "Switch to p" projectile-switch-project)
-    ("s" "Switch to open p" projectile-switch-open-project)
-    ("d" "Open p in dirvish" projectile-dired)
-    ("r" "Recent p files" projectile-recentf)]
-   [""
-    ("n" "Next p buffer" projectile-next-project-buffer)
-    ("p" "Previous p buffer" projectile-previous-project-buffer)
-    ("S" "Save all p buffers" projectile-save-project-buffers)
-    ("X" "Kill all p buffers" projectile-kill-buffers)
-    ("f" "Find references in p" projectile-find-references)]
-   [""
-    ("h" "Replace in p" projectile-replace)
-    ("g" "Ripgrep search in p" projectile-ripgrep)
-    ("m" "MisTTY Buffer @ p root" mistty-in-project)
-    ("C" "Clear known \'p\'s" projectile-clear-known-projects)
-    ("R" "Reset known \'p\'s" projectile-reset-known-projects)]])
-(keymap-global-set "C-c t" #'user/projectile-treemacs-anywhere-dispatch)
 
 
 (provide '05-project-management)
