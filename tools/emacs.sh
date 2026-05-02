a225c732dd02029a58802434c2426012337cf700
Colin Loeffler <that1guycolin@gmail.com>
2026-05-02 12:01:02 -0500

    Update: Emacsclient in x11 now uses gtk instead of lucid.

--- tools/emacs.sh
+++ tools/emacs.sh
@@ -3,16 +3,16 @@
 
 case "$XDG_SESSION_TYPE" in
     "x11")
-	/usr/local/emacs-gtk/bin/emacs-gtk $@
-	;;
+        /usr/local/emacs-gtk/bin/emacs-gtk $@
+        ;;
     "wayland")
-	/usr/local/emacs-pgtk/bin/emacs-pgtk $@
-	;;
+        /usr/local/emacs-pgtk/bin/emacs-pgtk $@
+        ;;
     "tty")
-	/usr/local/emacs-lucid/bin/emacs-lucid -nw $@
-	;;
+        /usr/local/emacs-lucid/bin/emacs-lucid -nw $@
+        ;;
     *)
-	echo "XDG_SESSION_TYPE=$XDG_SESSION_TYPE."
-	exit 42
-	;;
+        echo "XDG_SESSION_TYPE=$XDG_SESSION_TYPE."
+        exit 42
+        ;;
 esac
--- tools/emacsclient.sh
+++ tools/emacsclient.sh
@@ -3,27 +3,28 @@
 
 case "$XDG_SESSION_TYPE" in
     "x11")
-	BUILD="lucid"
-	;;
+        BUILD="gtk"
+        ;;
     "wayland")
-	BUILD="pgtk"
-	;;
+        BUILD="pgtk"
+        ;;
     "tty")
-	BUILD="lucid"
-	;;
+        BUILD="lucid"
+        ;;
     *)
-	echo "XDG_SESSION_TYPE=$XDG_SESSION_TYPE."
-	echo "Allowed options are x11, tty, & wayland."
-	exit 42
-	;;
+    echo "XDG_SESSION_TYPE=$XDG_SESSION_TYPE."
+    echo "Allowed options are x11, tty, & wayland."
+    exit 42
+    ;;
 esac
 
 #shellcheck disable=SC2086
 STATUS="$(systemctl --user is-active "emacs-${XDG_SESSION_TYPE}")"
+COMMAND="/usr/local/emacs-${BUILD}/bin/emacsclient-${BUILD}"
 
-if [[ "$STATUS" == "active" ]]; then
-    "/usr/local/emacs-${BUILD}/bin/emacsclient-${BUILD}" "$@"
+if [ "$STATUS" = "active" ]; then
+    "$COMMAND" "$@"
 else
     systemctl --user start "emacs-${XDG_SESSION_TYPE}"
-    "/usr/local/emacs-${BUILD}/bin/emacsclient-${BUILD}" "$@"
+    "$COMMAND" "$@"
 fi
