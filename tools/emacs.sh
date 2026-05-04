8d27a67a03557c1847443a84d7747279f5241b15
Colin Loeffler <that1guycolin@gmail.com>
2026-05-04 00:08:00 -0500

    Updates to scripts and systemd-unit files in tools.

--- tools/emacs.sh
+++ tools/emacs.sh
@@ -3,16 +3,17 @@
 
 case "$XDG_SESSION_TYPE" in
     "x11")
-        /usr/local/emacs-gtk/bin/emacs-gtk $@
+        /usr/local/emacs-gtk/bin/emacs $@
         ;;
     "wayland")
-        /usr/local/emacs-pgtk/bin/emacs-pgtk $@
+        /usr/local/emacs-pgtk/bin/emacs $@
         ;;
     "tty")
-        /usr/local/emacs-lucid/bin/emacs-lucid -nw $@
+        /usr/local/emacs-lucid/bin/emacs -nw $@
         ;;
     *)
-        echo "XDG_SESSION_TYPE=$XDG_SESSION_TYPE."
-        exit 42
-        ;;
+    echo "XDG_SESSION_TYPE=${XDG_SESSION_TYPE}."
+    echo "Typical values are 'x11' or 'wayland'."
+    exit 42
+    ;;
 esac
--- tools/emacsclient.sh
+++ tools/emacsclient.sh
@@ -8,11 +8,8 @@ case "$XDG_SESSION_TYPE" in
     "wayland")
         BUILD="pgtk"
         ;;
-    "tty")
-        BUILD="lucid"
-        ;;
     *)
-    echo "XDG_SESSION_TYPE=$XDG_SESSION_TYPE."
+    echo "XDG_SESSION_TYPE=${XDG_SESSION_TYPE}."
     echo "Allowed options are x11, tty, & wayland."
     exit 42
     ;;
@@ -20,11 +17,7 @@ esac
 
 #shellcheck disable=SC2086
 STATUS="$(systemctl --user is-active "emacs-${XDG_SESSION_TYPE}")"
-COMMAND="/usr/local/emacs-${BUILD}/bin/emacsclient-${BUILD}"
 
-if [ "$STATUS" = "active" ]; then
-    "$COMMAND" "$@"
-else
-    systemctl --user start "emacs-${XDG_SESSION_TYPE}"
-    "$COMMAND" "$@"
-fi
+[ "$STATUS" = "active" ] || systemctl --user start "emacs-${XDG_SESSION_TYPE}"
+/usr/local/emacs-${BUILD}/bin/emacsclient $@
+
