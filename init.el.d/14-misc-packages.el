4b2bc36292c2310ef89ebf3064fca0badec773d7
Colin Loeffler <that1guycolin@gmail.com>
2026-05-14 17:00:14 -0500

    Added package casual.

--- init.el.d/14-misc-packages.el
+++ init.el.d/14-misc-packages.el
@@ -1,7 +1,7 @@
 ;;; 14-misc-packages.el --- Misc & Dashboard -*- lexical-binding: t; -*-
 
 ;;; Packages included:
-;; dashboard, emacs-everywhere, free-keys, popper, telega
+;; casual, dashboard, emacs-everywhere, free-keys, popper, telega
 
 ;;; Commentary:
 ;; Packages that don't fit nicely into another category or, as is the case with
@@ -76,6 +76,194 @@
         '((name . "emacs-everywhere") (width . 80) (height . 20)
 	  (menu-bar-lines . 0) (tool-bar-lines . 0)
 	  (vertical-scroll-bars . nil))))
+
+
+;; =======  CASUAL  =======
+;; Setup/account for functions/variables
+(defvar org-agenda-mode-map)
+(defvar calc-mode-map)
+(defvar calc-alg-ent-map)
+(defvar calendar-mode-map)
+(defvar compilation-mode-map)
+(defvar grep-mode-map)
+(defvar css-mode-map)
+(defvar csv-mode-map)
+(defvar dired-mode-map)
+(defvar emacs-lisp-mode-map)
+(defvar eshell-mode-map)
+(defvar eww-mode-map)
+(defvar eww-bookmark-mode-map)
+(defvar ibuffer-mode-map)
+(defvar image-mode-map)
+(defvar Info-mode-map)
+(defvar isearch-mode-map)
+(defvar makefile-mode-map)
+(defvar org-mode-map)
+(defvar org-table-fedit-map)
+(defvar reb-mode-map)
+(defvar reb-lisp-mode-map)
+
+(declare-function org-agenda-clock-goto "org")
+
+(declare-function compilation-previous-error "compilation-mode")
+(declare-function compilation-next-error "compilation-mode")
+(declare-function compilation-display-error "compilation-mode")
+(declare-function compilation-previous-file "compilation-mode")
+(declare-function compilation-next-file "compilation-mode")
+
+(declare-function eww-browse-with-external-browser "eww")
+(declare-function shr-next-link "eww")
+(declare-function shr-previous-link "eww")
+(declare-function eww-previous-url "eww")
+(declare-function eww-next-url "eww")
+(declare-function eww-forward-url "eww")
+(declare-function eww-back-url "eww")
+(declare-function eww-bookmark-browse "eww")
+
+(declare-function ibuffer-backwards-next-marked "ibuffer")
+(declare-function ibuffer-forward-next-marked "ibuffer")
+(declare-function ibuffer-backward-filter-group "ibuffer")
+(declare-function ibuffer-forward-filter-group "ibuffer")
+(declare-function ibuffer-toggle-filter-group "ibuffer")
+(declare-function ibuffer-visit-buffer "ibuffer")
+(declare-function ibuffer-visit-buffer-other-window "ibuffer")
+
+(declare-function Info-history-back "Info")
+(declare-function Info-history-forward "Info")
+(declare-function Info-prev "Info")
+(declare-function Info-next-reference "Info")
+(declare-function Info-prev-reference "Info")
+(declare-function Info-next "Info")
+(declare-function Info-search "Info")
+
+;; Actual use-package object
+(use-package casual
+  :bind
+  (("C-o" . casual-editkit-main-tmenu)
+   (:map org-agenda-mode-map
+         ("C-o"   . casual-agenda-tmenu))
+   (:map calc-mode-map
+         ("C-o"   . casual-calc-tmenu))
+   (:map calc-alg-ent-map
+         ("C-o"   . casual-calc-tmenu))
+   (:map calendar-mode-map
+         ("C-o"   . casual-calendar))
+   (:map compilation-mode-map
+         ("C-o"   . casual-compile-tmenu))
+   (:map grep-mode-map
+         ("C-o"   . casual-compile-tmenu))
+   (:map css-mode-map
+         ("M-m"   . casual-css-tmenu))
+   (:map csv-mode-map
+         ("M-m"   . casual-csv-tmenu))
+   (:map dired-mode-map
+         ("C-o"   . casual-dired-tmenu))
+   (:map dired-mode-map
+         ("s"     . casual-dired-sort-by-tmenu))
+   (:map dired-mode-map
+         ("/"     . casual-dired-search-replace-tmenu))
+   (:map dired-mode-map
+         ("C-c e" . casual-dired-elisp-tmenu))
+   (:map emacs-lisp-mode-map
+         ("M-m"   . casual-elisp-tmenu))
+   (:map eshell-mode-map
+         ("C-o"   . casual-eshell-tmenu))
+   (:map eww-mode-map
+         ("C-o"   . casual-eww-tmenu))
+   (:map eww-bookmark-mode-map
+         ("C-o"   . casual-eww-bookmarks-tmenu))
+   (:map ibuffer-mode-map
+         ("C-o"   . casual-ibuffer-tmenu))
+   (:map ibuffer-mode-map
+         ("F"     . casual-ibuffer-filter-tmenu))
+   (:map ibuffer-mode-map
+         ("s"     . casual-ibuffer-sortby-tmenu))
+   (:map image-mode-map
+         ("C-o"   . casual-image-tmenu))
+   (:map Info-mode-map
+         ("C-o"   . casual-info-tmenu))
+   (:map isearch-mode-map
+         ("C-o"   . casual-isearch-tmenu))
+   (:map makefile-mode-map
+         ("M-m"   . casual-make-tmenu))
+   (:map org-mode-map
+         ("M-m"   . casual-org-tmenu))
+   (:map org-table-fedit-map
+         ("M-m"   . casual-org-table-fedit-tmenu))
+   (:map reb-mode-map
+         ("C-o"   . casual-re-builder-tmenu))
+   (:map reb-lisp-mode-map
+         ("C-o"   . casual-re-builder-tmenu)))
+  :functions
+  casual-ediff-install casual-ediff-tmenu casual-editkit-windows-tmenu
+  casual-editkit-rectangle-tmenu casual-editkit-registers-tmenu
+  casual-editkit-project-tmenu casual-lib-browse-forward-paragraph
+  casual-lib-browse-backward-paragraph casual-eww-backward-paragraph-link
+  casual-eww-forward-paragraph-link casual-info-browse-backward-paragraph
+  casual-info-browse-forward-paragraph
+  :defines ediff-mode-map
+  :config
+  (casual-ediff-install)
+  (add-hook 'ediff-keymap-setup-hook
+	    #'(lambda ()
+		(keymap-set ediff-mode-map "C-o" #'casual-ediff-tmenu)))
+
+  (bind-keys
+   ("C-c w"              . casual-editkit-windows-tmenu)
+   ("M-r"                . casual-editkit-rectangle-tmenu)
+   ("C-c g"              . casual-editkit-registers-tmenu)
+   ("C-c p"              . casual-editkit-project-tmenu)
+   :map org-agenda-mode-map
+   ("M-j"                . org-agenda-clock-goto)
+   ("J"                  . bookmark-jump)
+   :map compilation-mode-map
+   ("k"                  . compilation-previous-error)
+   ("j"                  . compilation-next-error)
+   ("o"                  . compilation-display-error)
+   ("["                  . compilation-previous-file)
+   ("]"                  . compilation-next-file)
+   :map grep-mode-map
+   ("k"                  . compilation-previous-error)
+   ("j"                  . compilation-next-error)
+   ("o"                  . compilation-display-error)
+   ("["                  . compilation-previous-file)
+   ("]"                  . compilation-next-file)
+   :map eww-mode-map
+   ("C-c C-o"            . eww-browse-with-external-browser)
+   ("j"                  . shr-next-link)
+   ("k"                  . shr-previous-link)
+   ("["                  . eww-previous-url)
+   ("]"                  . eww-next-url)
+   ("M-]"                . eww-forward-url)
+   ("M-["                . eww-back-url)
+   ("n"                  . casual-lib-browse-forward-paragraph)
+   ("p"                  . casual-lib-browse-backward-paragraph)
+   ("P"                  . casual-eww-backward-paragraph-link)
+   ("N"                  . casual-eww-forward-paragraph-link)
+   ("M-l"                . eww)
+   :map eww-bookmark-mode-map
+   ("p"                  . previous-line)
+   ("n"                  . next-line)
+   ("<double-mouse-1>"   . eww-bookmark-browse)
+   :map ibuffer-mode-map
+   ("{"                  . ibuffer-backwards-next-marked)
+   ("}"                  . ibuffer-forward-next-marked)
+   ("["                  . ibuffer-backward-filter-group)
+   ("]"                  . ibuffer-forward-filter-group)
+   ("$"                  . ibuffer-toggle-filter-group)
+   ("<double-mouse-1>"   . ibuffer-visit-buffer)
+   ("M-<double-mouse-1>" . ibuffer-visit-buffer-other-window)
+   :map Info-mode-map
+   ("M-["                . Info-history-back)
+   ("M-]"                . Info-history-forward)
+   ("p"                  . casual-info-browse-backward-paragraph)
+   ("n"                  . casual-info-browse-forward-paragraph)
+   ("h"                  . Info-prev)
+   ("j"                  . Info-next-reference)
+   ("k"                  . Info-prev-reference)
+   ("l"                  . Info-next)
+   ("/"                  . Info-search)
+   ("B"                  . bookmark-set)))
 
 
 ;; =======  DASHBOARD  =======
