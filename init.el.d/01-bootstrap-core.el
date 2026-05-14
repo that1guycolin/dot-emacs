529d0bca05eb10228346a443ee92938f7060201a
Colin Loeffler <that1guycolin@gmail.com>
2026-05-14 11:43:25 -0500

    Default header-args for :results no "value verbatim replace".

--- init.el.d/01-bootstrap-core.el
+++ init.el.d/01-bootstrap-core.el
@@ -140,12 +140,12 @@
   (setq org-src-lang-modes (assoc-delete-all "bash" org-src-lang-modes))
   (dolist (lang-mode-cons '(("bash" . bash-ts) ("cmake" . cmake-ts)
   			    ("json" . json-ts) ("lua" . lua-ts)
-  			    ("python" . python-ts) ("sh" . sh) ("toml" . toml-ts)
-  			    ("yaml" . yaml-ts) ("zsh" . sh)))
+  			    ("python" . python-ts) ("sh" . sh)
+			    ("toml" . toml-ts) ("yaml" . yaml-ts) ("zsh" . sh)))
     (add-to-list 'org-src-lang-modes lang-mode-cons))
 
   (setq org-babel-default-header-args
-	(cons '(:results . "value verbatim")
+	(cons '(:results . "value verbatim replace")
 	      (assq-delete-all :results org-babel-default-header-args)))
 
   (with-eval-after-load 'ob
