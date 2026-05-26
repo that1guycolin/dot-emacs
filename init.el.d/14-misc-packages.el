5d06e89eff3bf7a26f1dc8da86ee1586f4355ac3
Colin Loeffler <that1guycolin@gmail.com>
2026-05-26 08:19:13 -0500

    Updated casual & dashboard use-package objects to comply.

--- init.el.d/14-misc-packages.el
+++ init.el.d/14-misc-packages.el
@@ -16,6 +16,7 @@
 ;; ======================
 ;; Configuration is done primarly in DE.
 (use-package emacs-everywhere
+  :demand t
   :config
   ;; Customizing the frame appearance for a "popup" feel
   (setq emacs-everywhere-frame-parameters
@@ -80,65 +81,67 @@
 
 
 ;; =======  CASUAL  =======
-;; Setup/account for functions/variables
-(defvar org-agenda-mode-map)
-(defvar calc-mode-map)
-(defvar calc-alg-ent-map)
-(defvar calendar-mode-map)
-(defvar compilation-mode-map)
-(defvar grep-mode-map)
-(defvar css-mode-map)
-(defvar csv-mode-map)
-(defvar dired-mode-map)
-(defvar emacs-lisp-mode-map)
-(defvar eshell-mode-map)
-(defvar eww-mode-map)
-(defvar eww-bookmark-mode-map)
-(defvar ibuffer-mode-map)
-(defvar image-mode-map)
-(defvar Info-mode-map)
-(defvar isearch-mode-map)
-(defvar makefile-mode-map)
-(defvar org-mode-map)
-(defvar org-table-fedit-map)
-(defvar reb-mode-map)
-(defvar reb-lisp-mode-map)
 
-(declare-function org-agenda-clock-goto "org")
-
-(declare-function compilation-previous-error "compilation-mode")
-(declare-function compilation-next-error "compilation-mode")
-(declare-function compilation-display-error "compilation-mode")
-(declare-function compilation-previous-file "compilation-mode")
-(declare-function compilation-next-file "compilation-mode")
-
-(declare-function eww-browse-with-external-browser "eww")
-(declare-function shr-next-link "eww")
-(declare-function shr-previous-link "eww")
-(declare-function eww-previous-url "eww")
-(declare-function eww-next-url "eww")
-(declare-function eww-forward-url "eww")
-(declare-function eww-back-url "eww")
-(declare-function eww-bookmark-browse "eww")
-
-(declare-function ibuffer-backwards-next-marked "ibuffer")
-(declare-function ibuffer-forward-next-marked "ibuffer")
-(declare-function ibuffer-backward-filter-group "ibuffer")
-(declare-function ibuffer-forward-filter-group "ibuffer")
-(declare-function ibuffer-toggle-filter-group "ibuffer")
-(declare-function ibuffer-visit-buffer "ibuffer")
-(declare-function ibuffer-visit-buffer-other-window "ibuffer")
-
-(declare-function Info-history-back "Info")
-(declare-function Info-history-forward "Info")
-(declare-function Info-prev "Info")
-(declare-function Info-next-reference "Info")
-(declare-function Info-prev-reference "Info")
-(declare-function Info-next "Info")
-(declare-function Info-search "Info")
 
 ;; Actual use-package object
 (use-package casual
+  :defer t
+  :preface
+  (declare-function org-agenda-clock-goto "org")
+
+  (declare-function compilation-display-error "compilation-mode")
+  (declare-function compilation-next-error "compilation-mode")
+  (declare-function compilation-next-file "compilation-mode")
+  (declare-function compilation-previous-error "compilation-mode")
+  (declare-function compilation-previous-file "compilation-mode")
+  
+  (declare-function eww-back-url "eww")
+  (declare-function eww-bookmark-browse "eww")
+  (declare-function eww-browse-with-external-browser "eww")
+  (declare-function eww-forward-url "eww")
+  (declare-function eww-next-url "eww")
+  (declare-function eww-previous-url "eww")
+  (declare-function shr-next-link "eww")
+  (declare-function shr-previous-link "eww")
+  
+  (declare-function ibuffer-backward-filter-group "ibuffer")
+  (declare-function ibuffer-backwards-next-marked "ibuffer")
+  (declare-function ibuffer-forward-filter-group "ibuffer")
+  (declare-function ibuffer-forward-next-marked "ibuffer")
+  (declare-function ibuffer-toggle-filter-group "ibuffer")
+  (declare-function ibuffer-visit-buffer "ibuffer")
+  (declare-function ibuffer-visit-buffer-other-window "ibuffer")
+  
+  (declare-function Info-history-back "Info")
+  (declare-function Info-history-forward "Info")
+  (declare-function Info-next "Info")
+  (declare-function Info-next-reference "Info")
+  (declare-function Info-prev "Info")
+  (declare-function Info-prev-reference "Info")
+  (declare-function Info-search "Info")
+  
+  (defvar calc-alg-ent-map)
+  (defvar calc-mode-map)
+  (defvar calendar-mode-map)
+  (defvar compilation-mode-map)
+  (defvar css-mode-map)
+  (defvar csv-mode-map)
+  (defvar emacs-lisp-mode-map)
+  (defvar eshell-mode-map)
+  (defvar eww-bookmark-mode-map)
+  (defvar eww-mode-map)
+  (defvar grep-mode-map)
+  (defvar ibuffer-mode-map)
+  (defvar image-mode-map)
+  (defvar Info-mode-map)
+  (defvar isearch-mode-map)
+  (defvar makefile-mode-map)
+  (defvar org-agenda-mode-map)
+  (defvar org-mode-map)
+  (defvar org-table-fedit-map)
+  (defvar reb-lisp-mode-map)
+  (defvar reb-mode-map)
+
   :bind
   (("C-o" . casual-editkit-main-tmenu)
    (:map org-agenda-mode-map
@@ -157,14 +160,6 @@
          ("M-m"   . casual-css-tmenu))
    (:map csv-mode-map
          ("M-m"   . casual-csv-tmenu))
-   (:map dired-mode-map
-         ("C-o"   . casual-dired-tmenu))
-   (:map dired-mode-map
-         ("s"     . casual-dired-sort-by-tmenu))
-   (:map dired-mode-map
-         ("/"     . casual-dired-search-replace-tmenu))
-   (:map dired-mode-map
-         ("C-c e" . casual-dired-elisp-tmenu))
    (:map emacs-lisp-mode-map
          ("M-m"   . casual-elisp-tmenu))
    (:map eshell-mode-map
@@ -210,71 +205,82 @@
 		(keymap-set ediff-mode-map "C-o" #'casual-ediff-tmenu)))
 
   (bind-keys
-   ("C-c w"              . casual-editkit-windows-tmenu)
-   ("M-r"                . casual-editkit-rectangle-tmenu)
-   ("C-c g"              . casual-editkit-registers-tmenu)
-   ("C-c p"              . casual-editkit-project-tmenu)
+   ("C-c w"			 . casual-editkit-windows-tmenu)
+   ("M-r"			 . casual-editkit-rectangle-tmenu)
+   ("C-c g"			 . casual-editkit-registers-tmenu)
+   ("C-c p"			 . casual-editkit-project-tmenu)
    :map org-agenda-mode-map
-   ("M-j"                . org-agenda-clock-goto)
-   ("J"                  . bookmark-jump)
+   ("M-j"			 . org-agenda-clock-goto)
+   ("J"				 . bookmark-jump)
    :map compilation-mode-map
-   ("k"                  . compilation-previous-error)
-   ("j"                  . compilation-next-error)
-   ("o"                  . compilation-display-error)
-   ("["                  . compilation-previous-file)
-   ("]"                  . compilation-next-file)
+   ("k"				 . compilation-previous-error)
+   ("j"				 . compilation-next-error)
+   ("o"				 . compilation-display-error)
+   ("["				 . compilation-previous-file)
+   ("]"				 . compilation-next-file)
    :map grep-mode-map
-   ("k"                  . compilation-previous-error)
-   ("j"                  . compilation-next-error)
-   ("o"                  . compilation-display-error)
-   ("["                  . compilation-previous-file)
-   ("]"                  . compilation-next-file)
+   ("k"				 . compilation-previous-error)
+   ("j"				 . compilation-next-error)
+   ("o"				 . compilation-display-error)
+   ("["				 . compilation-previous-file)
+   ("]"				 . compilation-next-file)
    :map eww-mode-map
-   ("C-c C-o"            . eww-browse-with-external-browser)
-   ("j"                  . shr-next-link)
-   ("k"                  . shr-previous-link)
-   ("["                  . eww-previous-url)
-   ("]"                  . eww-next-url)
-   ("M-]"                . eww-forward-url)
-   ("M-["                . eww-back-url)
-   ("n"                  . casual-lib-browse-forward-paragraph)
-   ("p"                  . casual-lib-browse-backward-paragraph)
-   ("P"                  . casual-eww-backward-paragraph-link)
-   ("N"                  . casual-eww-forward-paragraph-link)
-   ("M-l"                . eww)
+   ("C-c C-o"			 . eww-browse-with-external-browser)
+   ("j"				 . shr-next-link)
+   ("k"				 . shr-previous-link)
+   ("["				 . eww-previous-url)
+   ("]"				 . eww-next-url)
+   ("M-]"			 . eww-forward-url)
+   ("M-["			 . eww-back-url)
+   ("n"				 . casual-lib-browse-forward-paragraph)
+   ("p"				 . casual-lib-browse-backward-paragraph)
+   ("P"				 . casual-eww-backward-paragraph-link)
+   ("N"				 . casual-eww-forward-paragraph-link)
+   ("M-l"			 . eww)
    :map eww-bookmark-mode-map
-   ("p"                  . previous-line)
-   ("n"                  . next-line)
-   ("<double-mouse-1>"   . eww-bookmark-browse)
+   ("p"				 . previous-line)
+   ("n"				 . next-line)
+   ("<double-mouse-1>"		 . eww-bookmark-browse)
    :map ibuffer-mode-map
-   ("{"                  . ibuffer-backwards-next-marked)
-   ("}"                  . ibuffer-forward-next-marked)
-   ("["                  . ibuffer-backward-filter-group)
-   ("]"                  . ibuffer-forward-filter-group)
-   ("$"                  . ibuffer-toggle-filter-group)
-   ("<double-mouse-1>"   . ibuffer-visit-buffer)
-   ("M-<double-mouse-1>" . ibuffer-visit-buffer-other-window)
+   ("{"				 . ibuffer-backwards-next-marked)
+   ("}"				 . ibuffer-forward-next-marked)
+   ("["				 . ibuffer-backward-filter-group)
+   ("]"				 . ibuffer-forward-filter-group)
+   ("$"				 . ibuffer-toggle-filter-group)
+   ("<double-mouse-1>"		 . ibuffer-visit-buffer)
+   ("M-<double-mouse-1>"	 . ibuffer-visit-buffer-other-window)
    :map Info-mode-map
-   ("M-["                . Info-history-back)
-   ("M-]"                . Info-history-forward)
-   ("p"                  . casual-info-browse-backward-paragraph)
-   ("n"                  . casual-info-browse-forward-paragraph)
-   ("h"                  . Info-prev)
-   ("j"                  . Info-next-reference)
-   ("k"                  . Info-prev-reference)
-   ("l"                  . Info-next)
-   ("/"                  . Info-search)
-   ("B"                  . bookmark-set)))
+   ("M-["			 . Info-history-back)
+   ("M-]"			 . Info-history-forward)
+   ("p"				 . casual-info-browse-backward-paragraph)
+   ("n"				 . casual-info-browse-forward-paragraph)
+   ("h"				 . Info-prev)
+   ("j"				 . Info-next-reference)
+   ("k"				 . Info-prev-reference)
+   ("l"				 . Info-next)
+   ("/"				 . Info-search)
+   ("B"				 . bookmark-set)))
 
 (use-package casual-avy
+  :defer t
   :bind ("M-g" . casual-avy-tmenu))
 
 
 ;; =======  DASHBOARD  =======
-(declare-function user/function-after-emacsclient-frame "01-bootstrap-core.el")
-
 (use-package dashboard
   :demand t
+  :preface
+  (declare-function user/function-after-emacsclient-frame
+		    "01-bootstrap-core.el")
+
+  (defun user/emacs-server-dashboard ()
+    "Refresh dashboard buffer after new emacsclient frame."
+    (user/function-after-emacsclient-frame #'dashboard-refresh-buffer))
+
+  :hook
+  ((elpaca-after-init . dashboard-insert-startupify-lists)
+   (elpaca-after-init . dashborad-initialize)
+   (server-after-make-frame . user/emacs-server-dashboard))
   :functions
   dashboard-insert-startupify-lists dashboard-initialize
   dashboard-setup-startup-hook dashboard-refresh-buffer
@@ -293,17 +299,10 @@
   (dashboard-items `((agenda   . 5)
 		     (projects . ,(length (project-known-project-roots)))
 		     (recents  . 5)))
-  
-  :config
-  (add-hook 'elpaca-after-init-hook #'dashboard-insert-startupify-lists)
-  (add-hook 'elpaca-after-init-hook #'dashboard-initialize)
-  (dashboard-setup-startup-hook)
-  (setq inhibit-redisplay nil)
 
-  (add-hook 'server-after-make-frame-hook
-	    #'(lambda ()
-		(user/function-after-emacsclient-frame
-		 #'dashboard-refresh-buffer))))
+  :config
+  (dashboard-setup-startup-hook)
+  (setq inhibit-redisplay nil))
 
 
 (provide '14-misc-packages)
