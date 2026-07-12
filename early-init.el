8e2c7ee754be5f3ade310d2826cfa36c2b0cdfaf
Colin Loeffler <that1guycolin@gmail.com>
2026-07-12 16:04:24 -0500

    feat(early-init): added eln-cache handling for no-littering

--- early-init.el
+++ early-init.el
@@ -140,6 +140,14 @@
 (when (fboundp 'tooltip-mode)
   (tooltip-mode -1))
 
+;;;; no-littering
+(when (and (fboundp 'startup-redirect-eln-cache)
+           (fboundp 'native-comp-available-p)
+           (native-comp-available-p))
+  (startup-redirect-eln-cache
+   (convert-standard-filename
+    (expand-file-name "var/eln-cache/" user-emacs-directory))))
+
 
 (provide 'early-init)
 ;;; early-init.el ends here
