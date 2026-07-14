733222b5eacc57ebb7ae899a2d39441631be60ab
Colin Loeffler <that1guycolin@gmail.com>
2026-07-14 06:59:57 -0500

    fix(no-littering): added explicit (require 'no-littering)

--- init.el
+++ init.el
@@ -79,9 +79,9 @@
 
 (defvar elpaca-order
   '(elpaca :repo "https://github.com/progfolio/elpaca.git"
-	   :ref nil :depth 1 :inherit ignore
-	   :files (:defaults "elpaca-test.el" (:exclude "extensions"))
-	   :build (:not elpaca-activate)))
+           :ref nil :depth 1 :inherit ignore
+           :files (:defaults "elpaca-test.el" (:exclude "extensions"))
+           :build (:not elpaca-activate)))
 (let* ((repo (expand-file-name "elpaca/" elpaca-sources-directory))
        (build (expand-file-name "elpaca/" elpaca-builds-directory))
        (order (cdr elpaca-order))
@@ -91,27 +91,27 @@
     (make-directory repo t)
     (when (<= emacs-major-version 28) (require 'subr-x))
     (condition-case-unless-debug err
-	(if-let* ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
-		  ((zerop
-		    (apply #'call-process
-			   `("git" nil ,buffer t "clone"
-			     ,@(when-let* ((depth (plist-get order :depth)))
-				 (list (format "--depth=%d" depth)
-				       "--no-single-branch"))
-			     ,(plist-get order :repo)
-			     ,repo))))
-		  ((zerop
-		    (call-process "git" nil buffer t "checkout"
-				  (or (plist-get order :ref) "--"))))
-		  (emacs (concat invocation-directory invocation-name))
-		  ((zerop
-		    (call-process emacs nil buffer nil
-				  "-Q" "-L" "." "--batch" "--eval"
-				  "(byte-recompile-directory \".\" 0 'force)")))
-		  ((require 'elpaca))
-		  ((elpaca-generate-autoloads "elpaca" repo)))
-	    (progn (message "%s" (buffer-string)) (kill-buffer buffer))
-	  (error "%s" (with-current-buffer buffer (buffer-string))))
+        (if-let* ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
+                  ((zerop
+                    (apply #'call-process
+                           `("git" nil ,buffer t "clone"
+                             ,@(when-let* ((depth (plist-get order :depth)))
+                                 (list (format "--depth=%d" depth)
+                                       "--no-single-branch"))
+                             ,(plist-get order :repo)
+                             ,repo))))
+                  ((zerop
+                    (call-process "git" nil buffer t "checkout"
+                                  (or (plist-get order :ref) "--"))))
+                  (emacs (concat invocation-directory invocation-name))
+                  ((zerop
+                    (call-process emacs nil buffer nil
+                                  "-Q" "-L" "." "--batch" "--eval"
+                                  "(byte-recompile-directory \".\" 0 'force)")))
+                  ((require 'elpaca))
+                  ((elpaca-generate-autoloads "elpaca" repo)))
+            (progn (message "%s" (buffer-string)) (kill-buffer buffer))
+          (error "%s" (with-current-buffer buffer (buffer-string))))
       ((error) (warn "%s" err) (delete-directory repo 'recursive))))
   (unless (require 'elpaca-autoloads nil t)
     (require 'elpaca)
@@ -121,7 +121,8 @@
 (elpaca `(,@elpaca-order))
 
 ;; No-littering
-(elpaca no-littering)
+(elpaca no-littering
+  (require 'no-littering))
 
 ;; Enable use-package integration with Elpaca
 (elpaca elpaca-use-package
