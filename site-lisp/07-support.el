e0775ca7db275951e5dd76098964cf533a1b07c7
Colin Loeffler <that1guycolin@gmail.com>
2026-08-23 04:10:11 -0500

    refactor(07-support): corrected location of telega-server on device

--- site-lisp/07-support.el
+++ site-lisp/07-support.el
@@ -900,9 +900,6 @@ doubles as a model-switcher."
               telega-highlight-text-mode telega-notifications-mode
               telega-root-auto-fill-mode telega-transient-keymaps-mode)
   :init (setq telega-use-images t)
-  :custom
-  (telega-server (expand-file-name "~/.guix/profile/bin/telega-server"))
-  (telega-server-libs-prefix "/home/colin-l/.guix-profile")
   :config
   (if (daemonp)
       (add-hook 'after-make-frame-functions
