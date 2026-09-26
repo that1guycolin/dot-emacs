86b7cba041b9349fd56b9da1e6fb9516dd57f379
Colin Loeffler <that1guycolin@gmail.com>
2026-09-26 05:31:48 -0500

    refactor(00-macros): added defcustom for that1guycolin/emacs-type

--- site-lisp/00-macros.el
+++ site-lisp/00-macros.el
@@ -5,6 +5,23 @@
 ;; init.
 
 ;;; Code:
+(eval-when-compile (require 'cl-lib))
+
+(defcustom that1guycolin/emacs-type nil
+  "The type of Emacs currently running.
+Acceptable values are \\='desktop, \\='termux, or \\='android-gui."
+  :type '(symbol)
+  :options '(desktop termux android-gui)
+  :group '(that1guycolin))
+
+(cond
+ ((and (eq system-type 'android) (null (getenv "TERMUX_VERSION")))
+  (setq that1guycolin/emacs-type 'android-gui))
+ ((eq system-type 'android)
+  (setq that1guycolin/emacs-type 'termux))
+ (t
+  (setq that1guycolin/emacs-type 'desktop)))
+
 (cl-defmacro that1guycolin/desktop-mobile (&key desk termux gui)
   "Set different options depending on where Emacs is active.
 DESK    - Settings for Emacs on PC/laptop.
@@ -13,12 +30,18 @@ GUI     - Settings for the Emacs Android GUI application (only required when
           the GUI and termux need different settings)."
   (declare (indent defun))
   `(cond
-    ((and (eq system-type 'android) (null (getenv "TERMUX_VERSION")))
+    ((eq that1guycolin/emacs-type 'android-gui)
      ,(or gui termux))
-    ((eq system-type 'android)
+    ((eq that1guycolin/emacs-type 'termux)
      ,(or termux desk))
+    ((eq that1guycolin/emacs-type 'desktop)
+     ,desk)
     (t
-     ,desk)))
+     (if that1guycolin/emacs-type
+         (error "\"that1guycolin/emacs-type\" set to %s"
+                that1guycolin/emacs-type)
+       (error "\"that1guycolin/emacs-type\ not set\"")))))
+
 
 
 (provide '00-macros)
